b659612a81a5fafb402ba47edd273587b3a39ca4
EER077 <nauryzbayev012@gmail.com>
2024-11-25 10:51:38 +0500

    Add files via upload

---
+++ IdeaProjects Module 11 Home Work/Module 11 Home Work/src/IHotelService.java
@@ -0,0 +1,5 @@
+import java.util.List;
+// Интерфейс для взаимодействия с UI-компонентом
+public interface IHotelService {
+    List<Hotel> searchHotels(String location, String classType, double maxPrice);
+}
